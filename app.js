145298a656f34c6ffbed12719c175a7b41066fcd
Alexey <redspirit@live.ru>
2024-09-23 13:31:05 +0300

    generate rigth colors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let {makeRequest} = require('./modules/Http');
 let Colors = require('./modules/Colors');
 let {bufferImage} = require('./modules/testImage');
 let fs = require('fs').promises;
+let colors = new Colors();
 
 setTimeout(async () => {
     // return;
@@ -11,20 +12,20 @@ setTimeout(async () => {
     // console.log('Done');
     // process.exit(0);
 
-    let colors = new Colors();
-
-    console.log(colors.splitGrayscaleByte(0x05));
-    // console.log(colors.splitGrayscaleByte(0xAF));
+    // console.log(colors.splitGrayscaleByte(0xFF, 0x00));
+    // console.log(colors.splitGrayscaleByte(0x00, 0x2E));
+    // console.log(colors.splitGrayscaleByte(0x07, 0x40));
     // console.log('1var', colors.compactBuffer(Buffer.from([0x00, 0x55])));
     // console.log('2var', colors.compactBuffer(Buffer.from([0x05, 0x05])));
     // console.log('res1', colors.decomp(0x0f));
     // console.log('res2', colors.decomp(0x33));
-    return;
+    // return;
 
     await colors.preparePalette('./palettes/palette1.png');
     let out = await colors.getImageBytes('./tmp/out.png');
 
     // return console.log(out);
+    // console.log(out);
 
     // let cols = colors.splitColors(encodedBuffer);
     // return console.log(cols);
@@ -34,8 +35,8 @@ setTimeout(async () => {
 
     // console.log(out.grayPart1)
 
-    await makeRequest('/display1', colors.compactBuffer(out.grayPart1));
-    await makeRequest('/display2', colors.compactBuffer(out.grayPart2));
+    await makeRequest('/display1', out.grayPart1);
+    await makeRequest('/display2', out.grayPart2);
     // await makeRequest('/display1', out.grayPart1);
     // await makeRequest('/display2', out.grayPart2);
     // await makeRequest(out.gray);
@@ -45,10 +46,28 @@ setTimeout(async () => {
 
 (async () => {
     return;
-    let part1 = await fs.readFile('./tmp/part1.bin');
-    let part2 = await fs.readFile('./tmp/part2.bin');
+    // let part1 = await fs.readFile('./tmp/part1.bin');
+    // let part2 = await fs.readFile('./tmp/part2.bin');
+    // await makeRequest('/display1', part1);
+    // await makeRequest('/display2', part2);
+
+    let allGray = await fs.readFile('./tmp/all.bin');
+    let part1Arr = [];
+    let part2Arr = [];
+    for(let i = 0; i < allGray.length; i +=2) {
+        let gsByte = colors.splitGrayscaleByte(allGray.readUInt8(i), allGray.readUInt8(i+1));
+        part1Arr.push(gsByte[0]);
+        part2Arr.push(gsByte[1]);
+    }
+    let part1 = Buffer.from(part1Arr);
+    let part2 = Buffer.from(part2Arr);
+
+    console.log('allGray', allGray);
+    console.log('part1', part1);
+    console.log('part2', part2);
+
     await makeRequest('/display1', part1);
     await makeRequest('/display2', part2);
-})()
+})();
 
 
--- modules/Colors.js
+++ modules/Colors.js
@@ -18,48 +18,12 @@ class Colors {
     splitGrayscaleByte (b1, b2) {
         let r1 = 0;
         let r2 = 0;
-        for(let i = 0; i < 8; i += 2) {
-            let bits2 = (b >> i) & 0b11;
-            let k = i;
-            if(bits2 === 1) {
-                r2 += 1 << k;
-            }
-            if(bits2 === 2) {
-                r1 += 1 << k;
-            }
-            if(bits2 === 3) {
-                r1 += 1 << k;
-                r2 += 1 << k;
-            }
+        let int16 = (b1 << 8) + b2;
+        for(let i = 0; i < 16; i += 2) {
+            if((int16 >> i) & 1) r1 += (1 << (i / 2));
+            if((int16 >> (i + 1)) & 1) r2 += (1 << (i / 2));
         }
-        return [r1,  r2];
-    }
-
-    compactBuffer(buf) {
-        let compactArr = [];
-        for(let i = 0; i < buf.length; i += 2) {
-            let color = buf.readUInt16BE(i);
-            let r = 0;
-            for(let k = 0; k < 8; k++) {
-                r += (1 & (color >> (k * 2))) << k;
-            }
-            compactArr.push(r);
-        }
-        return Buffer.from(compactArr);
-    }
-
-    decomp(byte) {
-        let byte1 = 0;
-        let byte2 = 0;
-
-        for (let i = 0; i < 8; i++) {
-            if (i >= 4) {
-                byte1 += ((byte >> i) & 1) << ((i - 4) * 2);
-            } else {
-                byte2 += ((byte >> i) & 1) << (i * 2);
-            }
-        }
-        return [byte1, byte2];
+        return [~r1, ~r2];
     }
 
     async getImageBytes(imageFile) {
@@ -72,49 +36,31 @@ class Colors {
             let color = bitmap.data.readUIntBE(i * 4, 3);
             let index = this.paletteColors.indexOf(color);
             if(index === -1) throw new Error(`image color #${color.toString(16)} not in palette`);
-            indexes.push(index);
+            indexes.push(3 - index);
         }
         let bytesArr = [];
-        let part1Arr = [];
-        let part2Arr = [];
         for(let i = 0; i < pixelsCount; i += 4) {
             let pixByte = 0;
             let onePixel = indexes.slice(i, i + 4);
             for(let j = 0; j < 4; j++) {
-                pixByte += onePixel[j] << (j * 2);
+                pixByte += onePixel[3-j] << (j * 2);
             }
-            pixByte = ~pixByte;
             bytesArr.push(pixByte);
+        }
 
-            let gsByte = this.splitGrayscaleByte(pixByte);
+        let part1Arr = [];
+        let part2Arr = [];
+        let bytesBuf = Buffer.from(bytesArr);
+        for(let i = 0; i < bytesBuf.length; i +=2) {
+            let gsByte = this.splitGrayscaleByte(bytesBuf.readUInt8(i), bytesBuf.readUInt8(i+1));
             part1Arr.push(gsByte[0]);
             part2Arr.push(gsByte[1]);
         }
         return {
-            full: Buffer.from(bytesArr),
+            full: bytesBuf,
             grayPart1: Buffer.from(part1Arr),
             grayPart2: Buffer.from(part2Arr),
         }
-        // let baseArr = []; // array for black/white colors bytes
-        // let grayArr = [] // array for 2-grays colors bytes
-        // for(let i = 0; i < pixelsCount; i += 8) {
-        //     let pixByteBase = 0;
-        //     let pixByteGray = 0;
-        //     let onePixel = indexes.slice(i, i + 8);
-        //     for(let j = 0; j < 8; j++) {
-        //         let p = onePixel[7-j];
-        //         // if(p === 3) pixByteBase += 0;
-        //         // if(p === 2) pixByteGray += 0;
-        //         if(p === 1) pixByteGray += 1 << j;
-        //         if(p === 0) pixByteBase += 1 << j;
-        //     }
-        //     baseArr.push(pixByteBase);
-        //     grayArr.push(pixByteGray);
-        // }
-        // return {
-        //     base: Buffer.from(baseArr),
-        //     gray: Buffer.from(grayArr),
-        // };
     }
 
     splitColors(fourColorBuffer) {
